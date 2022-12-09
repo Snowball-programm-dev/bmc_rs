0d96d26464b22070f6a7f602abb0d91014aba344
House3DXD <theguylol999@gmail.com>
2022-12-10 00:28:03 +0100

    text

--- src/main.rs
+++ src/main.rs
@@ -30,8 +30,30 @@ fn spawn_camera(mut commands: Commands) {
 fn spawn_basic_scene(
 	mut commands: Commands,
 	mut meshes: ResMut<Assets<Mesh>>,
-	mut materials: ResMut<Assets<StandardMaterial>>
+	mut materials: ResMut<Assets<StandardMaterial>>, 
+	asset_server: Res<AssetServer>
 ){
+	commands.spawn((
+		TextBundle::from_section(
+			"BMC.rs",
+			TextStyle {
+				font: asset_server.load(r"fonts\MyFont.ttf"),
+				font_size: 50.0,
+				color: Color::CYAN,
+			},
+		)
+		.with_text_alignment(TextAlignment::BOTTOM_CENTER)
+		.with_style(Style {
+            position_type: PositionType::Relative,
+            position: UiRect {
+                bottom: Val::Px(5.0),
+                right: Val::Px(15.0),
+                ..default()
+            },
+            ..default()
+        }),
+	));
+
 	commands.spawn(PbrBundle {
 		mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
 		material: materials.add(Color::rgb(0.3,0.5,0.3).into()),
